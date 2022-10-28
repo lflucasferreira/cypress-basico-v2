2dd74fa62849ba7fd4efa169ba617a9b52395c63
Lucas Ferreira da Silva <lferreira@questrade.com>
2022-10-28 20:13:25 -0300

    feat(viewport): add viewport configuration

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -173,4 +173,15 @@ describe('Central de Atendimento ao Cliente TAT', function () {
                 expect($input[0].files[0].name).to.equal('example.json')
             })
     });
+
+    it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+        cy.get('#privacy a')
+            .should('have.attr', 'target', '_blank')
+    });
+    
+    it('acessa a página da política de privacidade removendo o target e então clicanco no link', () => {
+        cy.get('#privacy a')
+            .invoke('removeAttr', 'target')
+            .click()
+    });
 })
